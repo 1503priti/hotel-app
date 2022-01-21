8af6d9c92c94d50b26cc08191f39fcc731306c4b
priti kumari <151085priti@gmai;.com>
2022-01-21 23:57:10 +0800

    intial commit

---
+++ src/test/java/com/hotelapp/rest/springhotelapprestapi/SpringHotelappRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelapp.rest.springhotelapprestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHotelappRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
