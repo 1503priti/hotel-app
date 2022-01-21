0b1e343fcbdb2d74cf99a66117425f6e9677bcbf
priti kumari <151085priti@gmai;.com>
2022-01-22 00:28:31 +0800

    java 9 added and appliaction start

--- src/main/java/com/hotelapp/SpringHotelappRestapiApplication.java
+++ src/main/java/com/hotelapp/SpringHotelappRestapiApplication.java
@@ -1,4 +1,4 @@
-package com.hotelapp.rest.springhotelapprestapi;
+package com.hotelapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
