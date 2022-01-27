1f688f67c653f79da89c5aa38fce444572e3353e
priti kumari <151085priti@gmai;.com>
2022-01-27 22:10:01 +0800

    add method to get list of hotels by menu,location, partner name

--- src/main/java/com/hotelapp/controllers/HotelController.java
+++ src/main/java/com/hotelapp/controllers/HotelController.java
@@ -21,49 +21,74 @@ import com.hotelapp.service.HotelService;
 @RestController
 @RequestMapping("/hotels-restapi")
 public class HotelController {
-	
+
 	@Autowired
 	HotelService hotelService;
-	
+
 	@PostMapping("/hotels")
-	public ResponseEntity<Hotel>  addHotel(@RequestBody Hotel hotel){
+	public ResponseEntity<Hotel> addHotel(@RequestBody Hotel hotel) {
 		Hotel newhotel = hotelService.addHotel(hotel);
 		HttpHeaders header = new HttpHeaders();
 		header.add("desc", "one hotel added");
-		return ResponseEntity.ok().headers(header).body(newhotel); //eithr create ResponseEntity instance or call the static methods
+		return ResponseEntity.ok().headers(header).body(newhotel); // eithr create ResponseEntity instance or call the
+																	// static methods
 	}
 
 	@PutMapping("/hotels")
-	public ResponseEntity<String> updateHotel(@RequestBody Hotel hotel){
+	public ResponseEntity<String> updateHotel(@RequestBody Hotel hotel) {
 		hotelService.updateHotel(hotel);
-		//return ResponseEntity.ok().body("updated");
+		// return ResponseEntity.ok().body("updated");
 		return ResponseEntity.ok("updated");
 	}
-	
-	@GetMapping("/hotels/hotel-by-id/{hotelId")
-	public ResponseEntity<Hotel> getHotelById(@PathVariable int hotelId){
+
+	@GetMapping("/hotels/hotel-by-id/{hotelId}")
+	public ResponseEntity<Hotel> getHotelById(@PathVariable int hotelId) {
 		Hotel hotel = hotelService.getHotelById(hotelId);
 		HttpHeaders header = new HttpHeaders();
 		header.add("desc", "getting one hotel instance");
-		return ResponseEntity.ok().headers(header).body(hotel); //eithr create ResponseEntity instance or call the static methods
-	
+		return ResponseEntity.ok().headers(header).body(hotel); // eithr create ResponseEntity instance or call the
+																// static methods
+
 	}
-	
+
 	@DeleteMapping("hotels/hotel-by-id/{hotelId}")
 	public ResponseEntity<Void> deleteHotel(@PathVariable int hotelId) {
-	    hotelService.deleteHotel(hotelId);
+		hotelService.deleteHotel(hotelId);
 		return ResponseEntity.ok().build();
-		
+
+	}
+
+	@GetMapping("/hotels/hotel-by-city/{cityName}")
+	public ResponseEntity<List<Hotel>> getHotelsByCity(@PathVariable("cityName") String city) {
+		List<Hotel> hotelList = 	hotelService.getHotelsByCity(city);
+		return ResponseEntity.ok().body(hotelList);
+
 	}
 	
+
+	@GetMapping("/hotels/hotel-by-menu/{menuName}")
+	public ResponseEntity<List<Hotel>> getHotelsByMenu(@PathVariable String menuName) {
+		List<Hotel> hotelList = 	hotelService.getHotelsByMenu(menuName);
+		return ResponseEntity.ok().body(hotelList);
+
+	}
+	@GetMapping("/hotels/hotel-by-delivery/{partnerName}")
+	public ResponseEntity<List<Hotel>> getHotelsByDelivery(@PathVariable String partnerName){
+		List<Hotel> hotelList = hotelService.getHotelsByDelivery(partnerName);
+		return ResponseEntity.ok().body(hotelList);
+	}
+	@GetMapping("/hotels/hotel-by-location/{location}")
+	public ResponseEntity<List<Hotel>> getHotelsByLocation(@PathVariable String location){
+		List<Hotel> hotelList = hotelService.getHotelsByLocation(location);
+		return ResponseEntity.ok().body(hotelList);
+	}
+	@GetMapping("/hotels/hotel-by-location/{location}/menuName/{menuName}")
+	public ResponseEntity<List<Hotel>> getHotelsByLocationAndMenu(@PathVariable String location,@PathVariable String menuName){
+		List<Hotel> hotelList = hotelService.getHotelsByLocationAndMenu(location, menuName);
+		return ResponseEntity.ok().body(hotelList);
+	}
 	/*
-	 * void updateHotel(Hotel hotel);
-	 * 
-	 * Hotel getHotelById(int hotelId);
-	 * 
-	 * void deleteHotel(int hotelId);
-	 * 
-	 * List<Hotel> getHotelsByCity(String city);
+	
 	 * 
 	 * List<Hotel> getHotelsByMenu(String menuName);
 	 * 
